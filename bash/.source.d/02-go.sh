8e3176831523d4ec6d2e694f39820b446801ed57
Brandon LeBlanc <jbleblanc@fb.com>
2020-08-14 19:58:02 -0500

    update go

--- bash/.source.d/02-go.sh
+++ bash/.source.d/02-go.sh
@@ -1,4 +1,20 @@
 #!/usr/bin/env bash
 
-@export GOPATH "$HOME/go"
-@export GO111MODULE on
+@setup-go() {
+    local -r gp=/data/users/$USER/gopaths
+
+    if [ -d "$gp" ]; then
+        @prepend-path GOPATH "$gp/gofbcode"
+        @prepend-path GOPATH "$gp/godeps"
+        @prepend-path GOPATH "$gp/gobuck"
+        @export GOROOT "$gp/goroot"
+        @prepend-path PATH "$gp/go-tools/bin"
+        @prepend-path PATH "$gp/goroot/bin"
+    else
+        @export GOPATH "$HOME/go"
+        @export GO111MODULE on
+    fi
+}
+
+@setup-go
+unset -f @setup-go
