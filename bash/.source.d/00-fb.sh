f7bcfcbfd330cc1206750963dce3673f37ab35bb
Brandon LeBlanc <jbleblanc@fb.com>
2021-04-21 16:27:54 -0700

    yolo: Derp, asset redirection in dev mode

--- bash/.source.d/00-fb.sh
+++ bash/.source.d/00-fb.sh
@@ -1,5 +1,23 @@
 #!/usr/bin/env bash
 
+alias stripcolors='sed "s/\x1B\[\([0-9]\{1,2\}\(;[0-9]\{1,2\}\)\?\)\?[mGK]//g"'
+
+# paste with title
+pwt() {
+  printf -v cmd '%q ' "$@"
+  eval "$cmd 2>&1" | pastry --title "$cmd"
+}
+
+# paste with title to stdout
+pwto() {
+  local tmpf
+  export PYTHONUNBUFFERED=x
+  tmpf=$(mktemp -q)
+  printf -v cmd '%q ' "$@"
+  trap 'pastry --title "$cmd" < "$tmpf"; rm -rf "$tmpf"' RETURN
+  eval "$cmd 2>&1" | tee "$tmpf"
+}
+
 @import /etc/bashrc
 @import /usr/facebook/ops/rc/master.bashrc
 @import "${HOME}/.fbchef/environment"
