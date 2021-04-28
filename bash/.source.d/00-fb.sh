a8c0d1a1c3ba0cc29a1813782d3226d41e38ee68
Brandon LeBlanc <jbleblanc@fb.com>
2021-04-28 13:52:02 -0700

    yolo: Continued development...

--- bash/.source.d/00-fb.sh
+++ bash/.source.d/00-fb.sh
@@ -18,6 +18,42 @@ pwto() {
   eval "$cmd 2>&1" | tee "$tmpf"
 }
 
+updatemymac() {
+  sudo bash -euxc '
+    /opt/facebook/bin/funsize && 
+    /opt/facebook/bin/fixmymac && 
+    /usr/local/bin/santactl sync --clean --debug
+  ' &&
+    /usr/bin/kdestroy &&
+    /usr/bin/kinit &&
+    /usr/local/bin/cc-certs &&
+    true
+}
+
+rebase() {
+  local -r bk="${1:-remote/master}"
+  local current_node parent_node dirty=0 public=0
+  if hg identify --id | grep --quiet '+'; then
+    dirty=1
+    hg shelve
+  fi
+
+  current_node=$(hg log -r . -T '{node}')
+  parent_node=$(hg log -r 'last(public() & ::.)' -T '{node}')
+  if [ "$current_node" == "$parent_node" ]; then
+    public=1
+  fi
+
+  hg pull
+  hg rebase -r 'draft()' -d "$bk"
+  if ((public)); then
+    hg checkout "$bk"
+  fi
+  if ((dirty)); then
+    hg unshelve
+  fi
+}
+
 @import /etc/bashrc
 @import /usr/facebook/ops/rc/master.bashrc
 @import "${HOME}/.fbchef/environment"
